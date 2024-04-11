7132691e88d9f055d5af29638973977d7a2b325b
Collin Morrison <morricollin@gmail.com>
2024-04-10 21:41:37 -0600

    Navbar is looking good, adding some text to the home page

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -2,10 +2,27 @@ import Link from "next/link"
 
 export default function Navbar() {
 
+    type portfolioItem = {
+        title: string,
+        description: string,
+        image: string,
+        link: string
+    }
+
+    const portfolioItems: portfolioItem[] = [
+        {
+            title: "AWS CDK Project",
+            description: "A project that uses the AWS Cloud Development Kit and React to create a serverless application",
+            image: "",
+            link: "https://github.com/CollinMorrison/FovusCodeChallenge"
+        },
+        
+    ]
+
     return (
         <div className="flex w-full justify-center pt-4">
-            <div className="w-11/12 h-12 bg-slate-950 opacity-50 flex justify-center sticky top-0 border-2 border-slate-500 rounded-full p-2">
-                <Link href="/contact">
+            <div className="w-11/12 h-12 bg-slate-950 opacity-50 flex justify-end sticky top-0 border-2 border-slate-500 rounded-full p-2">
+                <Link href="/contact" className="mr-5">
                     <p>Contact Me</p>
                 </Link>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import Navbar from "./components";
 export default function Home() {
   return (
     <main className="flex flex-col justify-between p-24">
+      <div>
+        <div className="text-9xl ml-20">Hi,</div>
+        <div className="text-9xl m-20">I'm Collin</div>
+      </div>
     </main>
   );
 }
